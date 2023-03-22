49a0c7de4490cfa52fbc6fc12511e07b4a28631d
Andrew Powers-Holmes <aholmes@omnom.net>
2023-03-22 15:16:48 +1100

    use run_pip for requirement install

--- install.py
+++ install.py
@@ -1,8 +1,4 @@
-import sys
-
-from launch import is_installed, run
-
+from launch import is_installed, run_pip
 
 if not is_installed("rembg"):
-    python = sys.executable
-    run(f'"{python}" -m pip install rembg', desc="Installing rembg", errdesc="Couldn't install rembg")
+    run_pip("install rembg", "requirements for rembgr")
